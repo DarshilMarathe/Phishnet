4158f42b954f287e774608de7b90055776b3876b
DarshilMarathe <darshilm1010@gmail.com>
2024-03-19 02:03:26 +0530

    Web-Application V1

---
+++ app.py
@@ -0,0 +1,67 @@
+#importing required libraries
+from flask import Flask, request, render_template
+from flask import jsonify
+import numpy as np
+import pandas as pd
+from sklearn import metrics 
+import warnings
+import pickle
+warnings.filterwarnings('ignore')
+from feature import FeatureExtraction
+
+file = open("pickle/model.pkl","rb")
+gbc = pickle.load(file)
+file.close()
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+
+        url = request.form["url"]
+        obj = FeatureExtraction(url)
+        x = np.array(obj.getFeaturesList()).reshape(1,30) 
+
+        y_pred =gbc.predict(x)[0]
+        #1 is safe       
+        #-1 is unsafe
+        y_pro_phishing = gbc.predict_proba(x)[0,0]
+        y_pro_non_phishing = gbc.predict_proba(x)[0,1]
+        # if(y_pred ==1 ):
+        pred = "It is {0:.2f} % safe to go ".format(y_pro_phishing*100)
+        print(y_pred)
+        print("1")
+        print(y_pro_phishing)
+        print("2")
+        print(y_pro_non_phishing)
+        return render_template('index.html',xx =round(y_pro_non_phishing,2),url=url )
+    return render_template("index.html", xx =-1)
+
+
+@app.route("/cextension", methods=["POST","GET"])
+def cext():
+    if request.method == "POST":
+        print(request.data)
+        print(type(request.data))
+        print()
+        print(request.json)
+        print(type(request.json))
+        dic = request.json
+        print(dic["checkurl"])
+        url = dic["checkurl"]
+        # url = request.form["checkurl"]
+        obj = FeatureExtraction(url)
+        x = np.array(obj.getFeaturesList()).reshape(1, 30)
+
+        y_pred = gbc.predict(x)[0]
+        y_pro_non_phishing = gbc.predict_proba(x)[0, 1]
+        return jsonify({"xx": round(y_pro_non_phishing, 2)})  # Send only the number
+        # return jsonify({"xx": "45"})
+
+    return jsonify({"xx": "1"})  # Default response
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
