58bc99d64336812c61c22fc588a1e5b085eaa70e
DarshilMarathe <darshilm1010@gmail.com>
2024-03-25 22:07:37 +0530

    Extension logic

--- Extension/popup.js
+++ Extension/popup.js
@@ -24,7 +24,25 @@ function modelsender(url){
     .then(data => {
         // Handle the response from the backend
         console.log(data)
-        document.getElementById("cc").innerHTML= data.xx;
+        
+        let x = data.xx;
+        let num = x*100;
+        if (0<=x && x<0.50){
+            num = 100-num;
+        }
+        let txtx = num.toString();
+        if(x<=1 && x>=0.50){
+            var label = "Website is "+txtx +"% safe to use";
+            document.getElementById("prediction").innerHTML = label;
+            // document.getElementById("button1").style.display="block";
+        }
+        else if (0<=x && x<0.50){
+            var label = "Website is "+txtx +"% unsafe to use"
+            document.getElementById("prediction").innerHTML = label ;
+            // document.getElementById("button2").style.display="block";
+        }
+
+        // document.getElementById("cc").innerHTML= data.xx;
     })
     .catch(error => {
         // Handle any errors
@@ -35,5 +53,4 @@ function modelsender(url){
     });
 
 }
-
 
